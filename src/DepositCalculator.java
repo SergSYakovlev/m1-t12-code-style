1fc7f285ffafcb9be2571e89c1d6043928ad6236
Serg S. Yakovlev <140004700+SergSYakovlev@users.noreply.github.com>
2023-08-22 18:58:48 +0300

    Applying of the Practicum's CodeStyle

---
+++ src/DepositCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class DepositCalculator {
+    double calculateComplexPercent(double amount, int depositPeriod) {
+        double pay = amount * Math.pow(1.005, 12.0 * depositPeriod);
+        return calculateAmount(pay);
+    }
+
+    double calculateSimplePercent(double amount, int depositPeriod) {
+        double pay = amount * (1 + 0.06 * depositPeriod);
+        return calculateAmount(pay);
+    }
+
+    double calculateAmount(double multiplier) {
+        double level = Math.pow(10, 2);
+        return Math.round(multiplier * level) / level;
+    }
+
+    void processMenu() {
+        int period;
+        int action;
+        int amount;
+        double outAmount;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму вклада в рублях:");
+        amount = scanner.nextInt();
+
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt();
+
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+
+        outAmount = 0;
+        if (action == 1) outAmount = calculateSimplePercent(amount, period);
+        else if (action == 2) outAmount = calculateComplexPercent(amount, period);
+
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outAmount);
+    }
+
+    public static void main(String[] args) {
+        new DepositCalculator().processMenu();
+    }
+
+
+}
